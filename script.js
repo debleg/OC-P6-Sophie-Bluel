81e55e7216aee34b4e3aa89195b21659c62653f7
degleg <legoff.d@outlook.com>
2024-06-29 18:06:16 +0200

    Add dynamic creation of works based on API call

--- script.js
+++ script.js
@@ -7,3 +7,40 @@ const fetchWorks = async () => {
         console.log("An error occurred:", error);
     };
 }
+
+//creates the container for the works to be displayed
+const gallery = document.querySelector(".gallery")
+
+//generates the works by iterating through the array
+const displayWorks = async () => {
+    try {
+        //the function needs to be called for the works to be returned
+        //works has to be stored in a variable as it doesn't exist outside of the function that generates it
+        const works = await fetchWorks();
+
+        works.forEach ((work)  =>  {
+            //creation of the 3 components for each work
+            const figure = document.createElement("figure");
+            const galleryImg = document.createElement("img");
+            const figcaption = document.createElement("figcaption");
+        
+            //retrieves img url to use as source
+            const galleryImgSrc = work.imageUrl;
+            galleryImg.src= galleryImgSrc;
+
+            //retrieves title to use as img alt and figcaption
+            const elementTitle = work.title;
+            galleryImg.alt= elementTitle;
+            figcaption.innerText= elementTitle;
+
+            //appends all elements, figure being the sub-container
+            gallery.appendChild(figure);
+            figure.appendChild(galleryImg);
+            figure.appendChild(figcaption);
+        })
+    } catch (error) {
+        console.log("An error occurred:", error)
+    }
+}
+
+displayWorks();
