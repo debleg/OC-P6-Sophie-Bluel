7e032b96d74c05eff244e3b096a7ef6ec1fa3bae
degleg <legoff.d@outlook.com>
2024-07-11 18:32:01 +0200

    Set up login script fully functional, with sessionStorage

--- login.js
+++ login.js
@@ -10,14 +10,6 @@ const loginEmailPattern = /^([\w\d\-_.]*)+@+([\w\d]*)+\.+([\w\d]){2,}$/;
 const loginPassword = document.getElementById("loginPassword");
 const loginPasswordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}$/;
 
-//the following are needed as an input to the API
-const loginCredentials = {
-  email: loginEmail.value,
-  password: loginPassword.value,
-};
-
-const login = JSON.stringify(loginCredentials);
-
 /**
  * Checks that the email value exists and matches the pattern defined previously
  */
@@ -25,7 +17,7 @@ function checkEmail() {
   loginEmail.addEventListener("input", () => {
     if (!loginEmail.value) {
       loginEmail.setCustomValidity("Veuillez remplir le champ Email");
-    } else if (!loginEmailPattern.test(loginEmail)) {
+    } else if (!loginEmailPattern.test(loginEmail.value)) {
       loginEmail.setCustomValidity("Veuillez entrer un email valide");
     } else {
       loginEmail.setCustomValidity("");
@@ -41,14 +33,12 @@ function checkPassword() {
   loginPassword.addEventListener("input", () => {
     if (!loginPassword.value) {
       loginPassword.setCustomValidity("Veuillez remplir le champ Mot de passe");
-    } else if (!loginPasswordPattern.test(loginPassword)) {
+    } else if (!loginPasswordPattern.test(loginPassword.value)) {
       loginPassword.setCustomValidity("Veuillez entrer un mot de passe valide");
     } else {
       loginPassword.setCustomValidity("");
-      
     }
     loginPassword.reportValidity();
-    console.log(loginPassword.reportValidity())
   });
 }
 
@@ -60,28 +50,40 @@ const loginAPI = async () => {
     const response = await fetch("http://localhost:5678/api/users/login", {
       method: "POST",
       headers: { "Content-type": "application/json" },
-      body: login,
+      body: JSON.stringify({
+        email: loginEmail.value,
+        password: loginPassword.value,
+      }),
     });
 
-    if (response.status === 200) {
-      const data = await response.json();
-      const token = data.token;
-      sessionStorage.setItem("token", token);
-      //above needs to be removed on logout click on main page (also switch names depending of token presence)
-
-      //redirects the user after a successful login
-      window.location.href = "index.html";
-    } else {
-      console.log(response.status);
-      alert("La connexion a échoué, veuillez vérifier vos identifiants");
-      window.location.href = "login.html"; //needed if the page keeps freezing
+    //allows token storage if token exists
+    const data = await response.json();
+    if (data && data.token) {
+      sessionStorage.setItem("token", data.token);
+    }
+    //handles API response codes, redirects to index if ok, shows alert if not
+    switch (response.status) {
+      case 200:
+        //redirects the user after a successful login
+        window.location.href = "index.html";
+        break;
+      case 401:
+        alert(
+          "La connexion avec ces identifiants n'est pas autorisée, veuillez entrer d'autres identifiants"
+        );
+        break;
+      case 404:
+        alert(
+          "Ces identifiants ne correspondent pas à un utilisateur existant, veuillez réessayer"
+        );
+        break;
+      default:
+        alert("Une erreur est survenue, veuillez réessayer ultérieurement");
     }
   } catch (error) {
     console.log("An error occurred:", error);
   }
 };
-
-//this needs an export? 3.3
 
 /**
  * Runs the form data checks and API login
@@ -98,7 +100,7 @@ function userLogin() {
         //communicates with the API
         loginAPI();
       } else {
-        console.log("The form fields do not contain the required information");
+        alert("Les champs ne correspondent pas à ceux attendus");
       }
     });
   } catch (error) {
